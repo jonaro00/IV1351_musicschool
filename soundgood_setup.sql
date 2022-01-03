5e80b1aa7bc92d71d1bb7ec8cca093aa6f5549af
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2022-01-03 17:11:23 +0100

    Task 2 Setup and test data

---
+++ soundgood_setup.sql
@@ -0,0 +1,190 @@
+
+DROP TABLE IF EXISTS contact_details CASCADE;
+CREATE TABLE contact_details (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ email VARCHAR(50) UNIQUE NOT NULL
+);
+
+ALTER TABLE contact_details ADD CONSTRAINT PK_contact_details PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS personal_details CASCADE;
+CREATE TABLE personal_details (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ ssn VARCHAR(30) UNIQUE NOT NULL,
+ name VARCHAR(50)
+);
+
+ALTER TABLE personal_details ADD CONSTRAINT PK_personal_details PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS phone CASCADE;
+CREATE TABLE phone (
+ contact_details_id INT,
+ phone VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE phone ADD CONSTRAINT PK_phone PRIMARY KEY (contact_details_id,phone);
+
+
+DROP TABLE IF EXISTS rental_instrument CASCADE;
+CREATE TABLE rental_instrument (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ type VARCHAR(50) NOT NULL,
+ brand VARCHAR(30),
+ price FLOAT(10) NOT NULL
+);
+
+ALTER TABLE rental_instrument ADD CONSTRAINT PK_rental_instrument PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS student CASCADE;
+CREATE TABLE student (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ instrument VARCHAR(50),
+ ensemble VARCHAR(50) CHECK (instrument IS NOT NULL OR ensemble IS NOT NULL),
+ skill_level VARCHAR(12) NOT NULL,
+ street VARCHAR(30),
+ city VARCHAR(30),
+ zip VARCHAR(10),
+ personal_details_id INT,
+ contact_details_id INT,
+ parent_contact_details_id INT
+);
+
+ALTER TABLE student ADD CONSTRAINT PK_student PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS time_period CASCADE;
+CREATE TABLE time_period (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ start_time TIMESTAMP(6) NOT NULL,
+ end_time TIMESTAMP(6) NOT NULL
+);
+
+ALTER TABLE time_period ADD CONSTRAINT PK_time_period PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS application CASCADE;
+CREATE TABLE application (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ student_id INT,
+ enrollment_offered BIT(1),
+ accepted BIT(1)
+);
+
+ALTER TABLE application ADD CONSTRAINT PK_application PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS instructor CASCADE;
+CREATE TABLE instructor (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ personal_details_id INT,
+ contact_details_id INT
+);
+
+ALTER TABLE instructor ADD CONSTRAINT PK_instructor PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS instructor_ensemble CASCADE;
+CREATE TABLE instructor_ensemble (
+ instructor_id INT,
+ ensemble VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE instructor_ensemble ADD CONSTRAINT PK_instructor_ensemble PRIMARY KEY (instructor_id,ensemble);
+
+
+DROP TABLE IF EXISTS instructor_instrument CASCADE;
+CREATE TABLE instructor_instrument (
+ instructor_id INT,
+ instrument VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE instructor_instrument ADD CONSTRAINT PK_instructor_instrument PRIMARY KEY (instructor_id,instrument);
+
+
+DROP TABLE IF EXISTS instructor_time_period CASCADE;
+CREATE TABLE instructor_time_period (
+ instructor_id INT,
+ time_period_id INT
+);
+
+ALTER TABLE instructor_time_period ADD CONSTRAINT PK_instructor_time_period PRIMARY KEY (instructor_id,time_period_id);
+
+
+DROP TABLE IF EXISTS lesson CASCADE;
+CREATE TABLE lesson (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ instrument VARCHAR(50),
+ ensemble VARCHAR(50) CHECK (instrument IS NOT NULL OR ensemble IS NOT NULL),
+ skill_level VARCHAR(12) NOT NULL,
+ min_slots INT,
+ max_slots INT CHECK (max_slots >= min_slots),
+ instructor_id INT,
+ time_period_id INT,
+ price FLOAT(10) NOT NULL
+);
+
+ALTER TABLE lesson ADD CONSTRAINT PK_lesson PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS rental CASCADE;
+CREATE TABLE rental (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ student_id INT,
+ rental_instrument_id INT,
+ time_period_id INT
+);
+
+ALTER TABLE rental ADD CONSTRAINT PK_rental PRIMARY KEY (id);
+
+
+DROP TABLE IF EXISTS booking CASCADE;
+CREATE TABLE booking (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ student_id INT,
+ lesson_id INT
+);
+
+ALTER TABLE booking ADD CONSTRAINT PK_booking PRIMARY KEY (id);
+
+
+ALTER TABLE phone ADD CONSTRAINT FK_phone_0 FOREIGN KEY (contact_details_id) REFERENCES contact_details (id) ON DELETE CASCADE;
+
+
+ALTER TABLE student ADD CONSTRAINT FK_student_0 FOREIGN KEY (personal_details_id) REFERENCES personal_details (id) ON DELETE CASCADE;
+ALTER TABLE student ADD CONSTRAINT FK_student_1 FOREIGN KEY (contact_details_id) REFERENCES contact_details (id) ON DELETE CASCADE;
+ALTER TABLE student ADD CONSTRAINT FK_student_2 FOREIGN KEY (parent_contact_details_id) REFERENCES contact_details (id) ON DELETE CASCADE;
+
+
+ALTER TABLE application ADD CONSTRAINT FK_application_0 FOREIGN KEY (student_id) REFERENCES student (id) ON DELETE CASCADE;
+
+
+ALTER TABLE instructor ADD CONSTRAINT FK_instructor_0 FOREIGN KEY (personal_details_id) REFERENCES personal_details (id) ON DELETE CASCADE;
+ALTER TABLE instructor ADD CONSTRAINT FK_instructor_1 FOREIGN KEY (contact_details_id) REFERENCES contact_details (id) ON DELETE CASCADE;
+
+
+ALTER TABLE instructor_ensemble ADD CONSTRAINT FK_instructor_ensemble_0 FOREIGN KEY (instructor_id) REFERENCES instructor (id) ON DELETE CASCADE;
+
+
+ALTER TABLE instructor_instrument ADD CONSTRAINT FK_instructor_instrument_0 FOREIGN KEY (instructor_id) REFERENCES instructor (id) ON DELETE CASCADE;
+
+
+ALTER TABLE instructor_time_period ADD CONSTRAINT FK_instructor_time_period_0 FOREIGN KEY (instructor_id) REFERENCES instructor (id) ON DELETE CASCADE;
+ALTER TABLE instructor_time_period ADD CONSTRAINT FK_instructor_time_period_1 FOREIGN KEY (time_period_id) REFERENCES time_period (id) ON DELETE CASCADE;
+
+
+ALTER TABLE lesson ADD CONSTRAINT FK_lesson_0 FOREIGN KEY (instructor_id) REFERENCES instructor (id) ON DELETE CASCADE;
+ALTER TABLE lesson ADD CONSTRAINT FK_lesson_1 FOREIGN KEY (time_period_id) REFERENCES time_period (id) ON DELETE CASCADE;
+
+
+ALTER TABLE rental ADD CONSTRAINT FK_rental_0 FOREIGN KEY (student_id) REFERENCES student (id) ON DELETE CASCADE;
+ALTER TABLE rental ADD CONSTRAINT FK_rental_1 FOREIGN KEY (rental_instrument_id) REFERENCES rental_instrument (id) ON DELETE CASCADE;
+ALTER TABLE rental ADD CONSTRAINT FK_rental_2 FOREIGN KEY (time_period_id) REFERENCES time_period (id) ON DELETE CASCADE;
+
+
+ALTER TABLE booking ADD CONSTRAINT FK_booking_0 FOREIGN KEY (student_id) REFERENCES student (id) ON DELETE CASCADE;
+ALTER TABLE booking ADD CONSTRAINT FK_booking_1 FOREIGN KEY (lesson_id) REFERENCES lesson (id) ON DELETE CASCADE;
+
+
---
+++ soundgood_test_data.sql
@@ -0,0 +1,213 @@
+
+--parent 1
+INSERT INTO contact_details (email) VALUES ('apparent@not.real');
+INSERT INTO phone VALUES (1, '+4612345678');
+
+--student 1
+INSERT INTO contact_details (email) VALUES ('first_student@soundgood.music');
+INSERT INTO phone VALUES (2, '+4612345678'), (2, '+4612345679');
+INSERT INTO personal_details (ssn, name) VALUES ('19970303-1234', 'Legvi McBoo');
+INSERT INTO student (
+    instrument,
+    ensemble,
+    skill_level,
+    street,
+    city,
+    zip,
+    personal_details_id,
+    contact_details_id,
+    parent_contact_details_id
+) VALUES (
+    'Oboe',
+    'Jazz',
+    'Beginner',
+    'Road 1',
+    'Townsville',
+    '1337',
+    1,
+    2,
+    1
+);
+
+--student 2, a sibling to student 1 since they have the same parent contact details
+INSERT INTO contact_details (email) VALUES ('second_student@soundgood.music');
+INSERT INTO phone VALUES (3, '+4612345678'), (3, '+4612345680');
+INSERT INTO personal_details (ssn, name) VALUES ('19990403-1234', 'Kanad McBoo');
+INSERT INTO student (
+    instrument,
+    ensemble,
+    skill_level,
+    street,
+    city,
+    zip,
+    personal_details_id,
+    contact_details_id,
+    parent_contact_details_id
+) VALUES (
+    'Flute',
+    NULL,
+    'Beginner',
+    'Road 1',
+    'Townsville',
+    '1337',
+    2,
+    3,
+    1
+);
+
+--instructor 1
+INSERT INTO contact_details (email) VALUES ('the_boss@soundgood.music');
+INSERT INTO phone VALUES (4, '+4612335632');
+INSERT INTO personal_details (ssn, name) VALUES ('19670201-1234', 'Krabi Macronn');
+INSERT INTO instructor (personal_details_id, contact_details_id) VALUES (3, 4);
+INSERT INTO instructor_instrument VALUES
+    (1, 'Guitar'),
+    (1, 'Oboe'),
+    (1, 'Otamatone'),
+    (1, 'Drums')
+;
+INSERT INTO instructor_ensemble VALUES
+    (1, 'RockBand'),
+    (1, 'Orchestra')
+;
+
+--instructor 2
+INSERT INTO contact_details (email) VALUES ('big_boi@soundgood.music');
+INSERT INTO phone VALUES (5, '+4612335876');
+INSERT INTO personal_details (ssn, name) VALUES ('19770211-1234', 'Inka Lao');
+INSERT INTO instructor (personal_details_id, contact_details_id) VALUES (4, 5);
+INSERT INTO instructor_instrument VALUES
+    (2, 'Guitar'),
+    (2, 'Drums'),
+    (2, 'Flute'),
+    (2, 'Violin')
+;
+INSERT INTO instructor_ensemble VALUES
+    (2, 'Jazz')
+;
+
+--times
+INSERT INTO time_period (start_time, end_time) VALUES
+    (TIMESTAMP '2022-01-03 08:00', TIMESTAMP '2022-01-03 10:00'),
+    (TIMESTAMP '2022-01-03 10:00', TIMESTAMP '2022-01-03 12:00'),
+    (TIMESTAMP '2022-01-03 13:00', TIMESTAMP '2022-01-03 15:00'),
+    (TIMESTAMP '2022-01-03 15:00', TIMESTAMP '2022-01-03 17:00'),
+    (TIMESTAMP '2022-01-04 08:00', TIMESTAMP '2022-01-04 10:00'),
+    (TIMESTAMP '2022-01-04 10:00', TIMESTAMP '2022-01-04 12:00'),
+    (TIMESTAMP '2022-01-04 13:00', TIMESTAMP '2022-01-04 15:00'),
+    (TIMESTAMP '2022-01-04 15:00', TIMESTAMP '2022-01-04 17:00'),
+    (TIMESTAMP '2022-02-02 08:00', TIMESTAMP '2022-02-02 10:00'),
+    (TIMESTAMP '2022-02-02 10:00', TIMESTAMP '2022-02-02 12:00'),
+    (TIMESTAMP '2022-02-02 13:00', TIMESTAMP '2022-02-02 15:00'),
+    (TIMESTAMP '2022-02-02 15:00', TIMESTAMP '2022-02-02 17:00')
+;
+
+--instructor times
+INSERT INTO instructor_time_period (instructor_id, time_period_id) VALUES
+    (1, 1),
+    (1, 2),
+    (1, 4),
+    (1, 5),
+    (1, 7),
+    (1, 8),
+    (2, 1),
+    (2, 5),
+    (2, 9)
+;
+
+--lesson 1: instructor 1 on 3 jan 08:00, booked by student 1
+INSERT INTO lesson (
+    instrument,
+    ensemble,
+    skill_level,
+    min_slots,
+    max_slots,
+    instructor_id,
+    time_period_id,
+    price
+) VALUES (
+    'Oboe',
+    NULL,
+    'Beginner',
+    1,
+    1,
+    1,
+    1,
+    100
+);
+INSERT INTO booking (student_id, lesson_id) VALUES (1, 1);
+
+--lesson 2: instructor 2 on 2 feb 08:00, booked by student 2
+INSERT INTO lesson (
+    instrument,
+    ensemble,
+    skill_level,
+    min_slots,
+    max_slots,
+    instructor_id,
+    time_period_id,
+    price
+) VALUES (
+    'Flute',
+    NULL,
+    'Beginner',
+    1,
+    1,
+    2,
+    9,
+    100
+);
+INSERT INTO booking (student_id, lesson_id) VALUES (2, 2);
+
+--parent 2
+INSERT INTO contact_details (email) VALUES ('mama@not.real');
+INSERT INTO phone VALUES (5, '+4612356784');
+
+--student 3
+INSERT INTO contact_details (email) VALUES ('smart_guy@soundgood.music');
+INSERT INTO phone VALUES (6, '+4612000001');
+INSERT INTO personal_details (ssn, name) VALUES ('19900405-1234', 'Bing Chilling');
+INSERT INTO student (
+    instrument,
+    ensemble,
+    skill_level,
+    street,
+    city,
+    zip,
+    personal_details_id,
+    contact_details_id,
+    parent_contact_details_id
+) VALUES (
+    'Saxophone',
+    'Jazz',
+    'Beginner',
+    'Road 2',
+    'Townsville',
+    '1337',
+    5,
+    6,
+    5
+);
+
+--lesson 3: Jazz ensemble with instructor 2 on 2 feb 13:00, booked by students 1 and 3
+INSERT INTO lesson (
+    instrument,
+    ensemble,
+    skill_level,
+    min_slots,
+    max_slots,
+    instructor_id,
+    time_period_id,
+    price
+) VALUES (
+    NULL,
+    'Jazz',
+    'Beginner',
+    2,
+    4,
+    2,
+    11,
+    150
+);
+INSERT INTO booking (student_id, lesson_id) VALUES (1, 3);
+INSERT INTO booking (student_id, lesson_id) VALUES (3, 3);
